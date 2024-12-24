f2e8b4d2d61ab09dbb83870de8db759ab6131631
Sagar-G3 <sagargundarapu62@gmail.com>
2024-12-24 15:25:05 +0530

    Updated Version

---
+++ index.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5,6,7,8];
+let evenOne = [];
+let oddOne = [];
+for (let i=0; i<arr.length; i++){
+    if(arr[i]%2==0){
+        evenOne+=arr[i]+" ";
+    }else{
+        oddOne+=arr[i]+" ";
+    }
+}
+console.log(evenOne);
+console.log(oddOne);
